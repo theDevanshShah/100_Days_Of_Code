c1d8f598427634c23ef8c375a7d0a9e18ab79569
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2025-08-16 02:45:10 +0530

    testing the branch creation feature from vs code

--- Code/Day0/hello-world.py
+++ Code/Day0/hello-world.py
@@ -4,3 +4,9 @@ print("Hii I am Dev here")
 print("Date : ", date.today())
 print("Time : ", time(10, 30))  # Example time
 print("I am learning Python with Dev and I am enjoying it a lot and I am excited to learn more and more")
+
+
+print("Hii --- IGNORE ---")
+print("This is a test message to check the functionality of the code editor.")
+print("This is a test message to check the functionality of the code editor.")
+print("This is a test message to check the functionality of the code editor.")
