e5e8b2766d40aab56a0dcc58880295b20a0b730d
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2025-09-10 21:14:55 +0530

    Practise done

---
+++ 10_Day_Commit_Plan/Day_1/practise.py
@@ -0,0 +1,23 @@
+'''
+Mini Problems for Practice
+	1.	Print Hello World on two lines using \n.
+	2.	Print 1,2,3,4,5 using print with sep.
+	3.	Print a table row: Name | Age | City without newlines.
+	4.	Print "She said "Python is fun"" using escape characters.
+	5.	Write Hello three times in a row on one line.
+'''
+
+# 1 Print Hello World on two lines using \n.
+print("Hello \n World")
+
+# 2 Print 1,2,3,4,5 using print with sep.
+print("1","2","3","4","5", sep=",")
+
+# 3 Print a table row: Name | Age | City without newlines.
+print("Name","Age","City","Gender", sep=" | ")
+
+# 4 Print "She said "Python is fun"" using escape characters.
+print("She said \"Python is fun\"")
+
+# 5 Write Hello three times in a row on one line.
+print("Hello " * 3)
