e1d177f0e8bf538a70faa191a5ba8b33420c6897
Mr. Devansh Shah <devansh.shah.de@gmail.com>
2025-09-10 17:22:14 +0530

    Refactor addition program to include error handling for invalid input

--- harward_cs50_Intro_To_Python/one.py
+++ harward_cs50_Intro_To_Python/one.py
@@ -1,6 +1,7 @@
-x = float(input("What's x ? "))
-y = float(input("What's y ? "))
-
-z = round(x + y)
-
-print(f"{z:,}")
+try:
+	x = float(input("What's x? "))
+	y = float(input("What's y? "))
+	z = round(x + y)
+	print(f"{z:,}")
+except ValueError:
+	print("Please enter valid numbers for x and y.")
